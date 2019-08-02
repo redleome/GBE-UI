c6a44b6cc93c2077545c9d8aa1c6d92b8e240a16
Angelica-Dimude <angelica.dimude@telenordigital.com>
2019-08-02 16:59:27 +0200

    working sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 $("#menu").click(function(e) {
         e.preventDefault();
         // $("#sidenav").toggleClass("d-none");
-        $("#sidenav").toggleClass("d-none");
+        // $("#sidenav").toggleClass("d-none");
+        // $("#sidenav").toggleClass("col-2 d-none");
+         $("#sidenav").toggleClass("collapsed");
+        // $("#content").toggleClass("col-12 col-9");
 });
